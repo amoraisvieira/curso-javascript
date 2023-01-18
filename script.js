94212cd3c217e584a169e55062a9fae2c27f5fca
Amanda de Morais Vieira <amanda.moraaisv@gmail.com>
2023-01-18 19:24:36 -0300

    Concatenando arrays

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function sum(x, y, z) {
 
   console.log(sum(... numbers));
 
-
+  //Função que soma N valores
   function sum(... valores){
     return valores.reduce((acc, next) => acc + next);
   }
 
   console.log('soma:', sum(2,2,3));
+
+  //Concatenando arrays
+  const valores = [4, 1, 5, 7, 2];
+  const valores2 = [3, 5, 9, 4];
+
+  const array = [...valores, ...valores2]   //com arrays sempre se utiliza chaves
+  console.log('array: ' , array);
