3b8fa9ba32e445f9a24c8471ad51fc246cca04bf
Amanda de Morais Vieira <amanda.moraaisv@gmail.com>
2023-01-25 12:48:32 -0300

    Aula sobre shorthand method names

---
+++ desestruturacao_array.js
@@ -0,0 +1,12 @@
+let [a, b] = ['x', 'y'];
+let obj = { a, b };
+console.log(obj);
+
+const nomes = {
+    name: 'Amanda',
+    Hello() {
+        console.log('Hello');
+    }
+};
+
+console.log(nomes.Hello());
